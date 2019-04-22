8b51c37a42cee0b85ff5d9539eb9124c5ac4ad85
Diego Marquez <marqueztdiego@gmail.com>
2019-04-22 17:45:11 -0300

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-discussion
-Discussion System
---
+++ precache-manifest.0b0ad65fc1214abc33efeed7596efcd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6881d71a40eab7d9a7ef",
+    "url": "/react-discussion/static/css/main.b008e4b6.chunk.css"
+  },
+  {
+    "revision": "6881d71a40eab7d9a7ef",
+    "url": "/react-discussion/static/js/main.6881d71a.chunk.js"
+  },
+  {
+    "revision": "0730e277c6dedccd7996",
+    "url": "/react-discussion/static/js/runtime~main.0730e277.js"
+  },
+  {
+    "revision": "cd3cc771e687b131c614",
+    "url": "/react-discussion/static/css/2.7a597382.chunk.css"
+  },
+  {
+    "revision": "cd3cc771e687b131c614",
+    "url": "/react-discussion/static/js/2.cd3cc771.chunk.js"
+  },
+  {
+    "revision": "eb20512af92a5d52e667dd14794ea23b",
+    "url": "/react-discussion/index.html"
+  }
+];
